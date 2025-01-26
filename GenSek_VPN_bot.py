1388573dc9ec7079d5f5b166445d125071334a2e
Геннадий Скалецкий <skaletzkiy.gena@yandex.ru>
2025-01-26 14:51:20 +0300

    Изменения в GenSek_VPN_bot.py

--- GenSek_VPN_bot.py
+++ GenSek_VPN_bot.py
@@ -1,50 +1,24 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, CallbackContext, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler
+import os
 
-# Счетчик
-counter = 0
+# Получаем токен из переменной окружения
+token = os.getenv('TELEGRAM_TOKEN')
 
-# Обработчик команды /start
-async def start(update: Update, context: CallbackContext) -> None:
-    # Создание кнопки
-    keyboard = [
-        [InlineKeyboardButton("Увеличить счетчик", callback_data='increase')],
-    ]
-    
-    # Создание разметки с кнопкой
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    
-    # Отправляем сообщение с кнопкой
-    await update.message.reply_text(f'Счетчик: {counter}', reply_markup=reply_markup)
-
-# Обработчик нажатия кнопки
-async def button(update: Update, context: CallbackContext) -> None:
-    global counter
-    query = update.callback_query
-    await query.answer()
-    
-    # Если кнопка была нажата, увеличиваем счетчик
-    if query.data == 'increase':
-        counter += 1
-    
-    # Обновляем сообщение с новым значением счетчика
-    await query.edit_message_text(text=f'Счетчик: {counter}', reply_markup=query.message.reply_markup)
+def start(update, context):
+    update.message.reply_text("Привет, я твой бот!")
 
 def main():
-    # Токен бота, полученный от BotFather
-    token = '7734456717:AAFeMyZlA_Nv4ZwHM6Qt3s7M9AhW_d1advE'  # Замените на свой токен
+    updater = Updater(token, use_context=True)
 
-    # Создание экземпляра Application
-    application = Application.builder().token(token).build()
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
 
-    # Добавление обработчиков команд
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CallbackQueryHandler(button))  # Обработчик кнопок
-
-    # Запуск бота
-    application.run_polling()
+    # Используем метод polling, который не требует открытия порта
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
     main()
+
 
 
